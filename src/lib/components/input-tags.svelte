43127f714e6bec7a5fa3da3b4a130572c93f62db
mibriggs <33757644+mibriggs@users.noreply.github.com>
2023-10-28 00:44:07 -0400

    moved toast plus removed js

--- src/lib/components/input-tags.svelte
+++ src/lib/components/input-tags.svelte
@@ -5,29 +5,29 @@
 	let ingredients: string[] = [];
 	let newIngredient: string = '';
 
+	const showToast = (message: string, icon: string) => {
+		toast(message, {
+			position: 'top-center',
+			duration: 1250,
+			icon: icon,
+			style:
+				'--tw-border-opacity: 1; border-color: rgb(13 148 136 / var(--tw-border-opacity)); border-width: 2px;'
+		});
+	};
+
 	const addIngredient = () => {
 		if (newIngredient.trim().length === 0) {
-			console.log('too short');
-			toast("Can't be empty", {
-				position: 'bottom-right',
-				duration: 1250,
-				icon: '✋🏾'
-			});
+			showToast("Can't be empty", '✋🏾');
 			return;
 		}
 
 		if (ingredients.indexOf(newIngredient.trim().toLocaleLowerCase()) !== -1) {
-			toast('Ingredient already added', {
-				position: 'bottom-right',
-				duration: 1000,
-				icon: '🙅🏾‍♂️'
-			});
+			showToast('Ingredient already added', '🙅🏾‍♂️');
 			return;
 		}
-		console.log(`Added ${newIngredient} to list`);
+
 		ingredients = [...ingredients, newIngredient.trim().toLocaleLowerCase()];
 		newIngredient = '';
-		console.log(ingredients);
 	};
 
 	const removeIngredient = (ingredientIndex: number) => {
@@ -38,23 +38,21 @@
 <div
 	class="flex w-5/6 flex-col gap-4 rounded-lg border-2 border-raisin px-2 py-3 md:w-1/2 xl:w-5/12"
 >
-	{#if ingredients.length > 0}
-		<div class="flex w-full flex-wrap gap-2">
-			{#each ingredients as ingredient, index (ingredient)}
-				<div
-					class=" flex items-center justify-center gap-2 rounded-lg bg-stone-200 px-2 py-1"
-					in:scale
-					out:blur
+	<div class="flex w-full flex-wrap gap-2 empty:hidden">
+		{#each ingredients as ingredient, index (ingredient)}
+			<div
+				class=" flex items-center justify-center gap-2 rounded-lg bg-stone-200 px-2 py-1"
+				in:scale
+				out:blur
+			>
+				<span>{ingredient}</span>
+				<button
+					class="flex h-6 w-6 items-center justify-center rounded-full bg-stone-500 text-snow"
+					on:click={() => removeIngredient(index)}>&times;</button
 				>
-					<span>{ingredient}</span>
-					<button
-						class="flex h-6 w-6 items-center justify-center rounded-full bg-stone-500 text-snow"
-						on:click={() => removeIngredient(index)}>&times;</button
-					>
-				</div>
-			{/each}
-		</div>
-	{/if}
+			</div>
+		{/each}
+	</div>
 
 	<form on:submit|preventDefault={addIngredient} class="w-full">
 		<input
